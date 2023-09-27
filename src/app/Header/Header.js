649ff6e273c6c58253dbb06f381b374fd1ea747f
Justin Lawson <jstn.lawson@gmail.com>
2023-09-27 15:24:27 -0500

    header logo dynamic sizing

--- src/app/Header/Header.js
+++ src/app/Header/Header.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import Image from 'next/image';
+import uglyLogo from 'public/images/uglyLogo.svg';
 
 const Header = () => {
   return (
-    <header>
-      <h1>Header</h1>
-      
+    <header className='bg-white h-20 sm:h-20 md:h-24 lg:h-32 xl:h-40'>
+      <div className="header-image-container ">
+        <Image
+          src={uglyLogo}
+          alt="ugly logo"
+          className="h-20 sm:h-20 md:h-24 lg:h-32 xl:h-40 float-left ml-6"
+        />
+      </div>
+
     </header>
   );
 };
