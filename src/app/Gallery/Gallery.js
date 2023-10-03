f0b4ecb4df647c36f5b29857392d7c9b4a8402a5
Justin Lawson <jstn.lawson@gmail.com>
2023-10-03 15:47:17 -0500

    layout corrected for large screen only on gallery

--- src/app/Gallery/Gallery.js
+++ src/app/Gallery/Gallery.js
@@ -8,20 +8,39 @@ import "./Gallery.css"
 export default function Gallery() {
 
     return (
-        <div className="gallery-container flex justify-center items-center">
-            <div className="gallery-text-contaner  bg-white">
-                <h1 className="text-northBlue">Have a Look</h1>
-                <p className="text-northBlue">North Harwood Floors completes every project with a skillful blend 
-                   of professional service and true craftsmanship.</p>
-                   <button className="blue-button">view the gallery →</button>
-            </div>
-            <div className="gallery-cover-image-container ">
-                <Image
-                    src={galleryCover}
-                    alt="Gallery Cover Image"
-                    quality={100}
-                />
-            </div>
-        </div>
+        <div className="gallery-container">
+      <div className="gallery-cover-image-container">
+        <Image
+          src={galleryCover}
+          alt="Gallery Cover Image"
+          quality={100}
+          layout="intrinsic"
+        />
+      </div>
+      <div className="gallery-text-container bg-white">
+        <h1 className="text-northBlue">Have a Look</h1>
+        <p className="text-northBlue">
+          North Harwood Floors completes every project with a skillful blend of
+          professional service and true craftsmanship.
+        </p>
+        <button className="blue-button">view the gallery →</button>
+      </div>
+    </div>
+        // <div className="gallery-container flex justify-center items-center">
+        //     <div className="gallery-text-contaner  bg-white">
+        //         <h1 className="text-northBlue">Have a Look</h1>
+        //         <p className="text-northBlue">North Harwood Floors completes every project with a skillful blend 
+        //            of professional service and true craftsmanship.</p>
+        //            <button className="blue-button">view the gallery →</button>
+        //     </div>
+        //     <div className="gallery-cover-image-container ">
+        //         <Image
+        //             src={galleryCover}
+        //             alt="Gallery Cover Image"
+        //             quality={100}
+        //             layout="intrinsic"
+        //         />
+        //     </div>
+        // </div>
     )
 }
