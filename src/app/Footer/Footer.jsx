32b1345768eb6434bf7b155f597a34e416ff9686
Justin Lawson <jstn.lawson@gmail.com>
2023-10-17 15:02:23 -0500

    started footer layout

--- src/app/Footer/Footer.jsx
+++ src/app/Footer/Footer.jsx
@@ -1,12 +1,34 @@
 import React from "react";
+import Image from "next/image";
+import uglyLogo from 'public/images/uglyLogo.svg'
+import './Footer.css';
 
 
-
-export default function Footer () {
+export default function Footer() {
     return (
-        <footer className=" bottom-0 w-full  text-white py-2 bg-northBlue ">
-            <h2>North Hardwood Floors LLC</h2>
-        </footer>
-        
+        // <footer>
+        <div className="footer-container flex h-auto items-center text-northBlue bg-northLightBlue">
+            <div>
+                <Image
+                    src={uglyLogo}
+                    alt="ugly logo"
+                    className="h-16 w-20 text-white"
+                />
+            </div>
+            <div className=" flex flex-col ml-32 mr-4">
+                <h2 className="text-xs">CONTACT</h2>
+               
+                    612-246-0052
+                 <span className="small-texts">    
+                    5340 Bloomington Ave
+                    Minneapolis MN, 55417
+                </span>
+            </div>
+            <div className="ml-4">
+                <h2 className="text-xs">CONNECT</h2>
+            </div>
+        </div>
+        // </footer>
+
     )
 }
