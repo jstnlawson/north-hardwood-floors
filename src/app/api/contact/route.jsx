6d9a5b7889d5bb25cca28cb47a42721288cd31d8
Justin Lawson <jstn.lawson@gmail.com>
2023-11-08 11:12:38 -0600

    updated email import path which was flagged on build

--- src/app/api/contact/route.jsx
+++ src/app/api/contact/route.jsx
@@ -1,7 +1,9 @@
 
 import { render } from "@react-email/components";
 import { transporter, smptEmail, smptPassword } from "@/utils/nodemailer";
-import { Email } from "@/app/Contact/Email";
+// import { Email } from "@/app/Contact/Email";
+import { Email } from "src/app/Contact/Email.jsx";
+
 
 
 export async function POST(req) {
@@ -29,31 +31,3 @@ export async function POST(req) {
         res.status(500).send("Internal Server Error: " + error.message);
     }
 }
-
-// export default async function handler(req, res) {
-//     if (req.method === 'POST') {
-//         const body = await req.json();
-//         const { name, email, city, serviceSuggestion, message } = body;
-
-//         const emailHtml = render(
-//             <Email name={name} email={email} city={city} serviceSuggestion={serviceSuggestion} message={message} />
-//         );
-
-//         const options = {
-//             from: smptEmail,
-//             to: smptEmail,
-//             subject: "New Project Request",
-//             html: emailHtml,
-//         };
-
-//         try {
-//             await transporter.sendMail(options);
-//             res.status(200).send("OK");
-//         } catch (error) {
-//             console.error("Failed to send email:", error);
-//             res.status(500).send("Internal Server Error: " + error.message);
-//         }
-//     } else {
-//         res.status(405).send("Method Not Allowed");
-//     }
-// }
