74c44268b69408653f497d24bc28d8fe35cc56ae
Justin Lawson <jstn.lawson@gmail.com>
2023-11-10 12:48:51 -0600

    js hamburger complete

--- src/app/Header/Header.jsx
+++ src/app/Header/Header.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import Image from 'next/image';
 import uglyLogo from 'public/images/uglyLogo.svg';
 import "./Header.css";
-import Hamburger from '@/app/Hamburger/Hamburger';
 import JsHamburger from '../JsHamburger/JsHamburber';
 
 const Header = ({ hideHeader }) => {
@@ -18,10 +17,6 @@ const Header = ({ hideHeader }) => {
   return (
     <header id='header' className={`sticky top-0 z-10 ${hideHeader ? 'hidden' : ''}`}>
       <div className="bg-northBlue fixed w-full flex items-center justify-between  p-2">
-        
-        {/* <div >
-          <Hamburger />
-        </div> */}
 
         <div>
           <JsHamburger />
--- src/app/JsHamburger/JsHamburber.jsx
+++ src/app/JsHamburger/JsHamburber.jsx
@@ -1,34 +1,73 @@
 import React, { useState } from 'react';
+import './JsHamburger.css';
 
 export default function JsHamburger() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  
-  const handleClick = () => {
-      setIsOpen(!isOpen);
-  };
-
-return(
+    const [isOpen, setIsOpen] = useState(false);
     
-  <button onClick={handleClick} 
-  className="flex flex-col justify-center items-center">
-    <span className={`bg-northBeige block transition-all duration-300 ease-out 
-                    h-1 w-[35px] rounded-sm ${isOpen ? 
-                    'rotate-45 translate-y-1' : '-translate-y-[5px]'
-                    }`} >
-    </span>
-    <span className={`bg-northBeige block transition-all duration-300 ease-out 
-                    h-1 w-[35px] rounded-sm my-[ ${isOpen ? 
-                    'opacity-0' : 'opacity-100'
-                    }`} >
-    </span>
-    <span className={`bg-northBeige block transition-all duration-300 ease-out 
-                    h-1 w-[35px] rounded-sm ${isOpen ? 
-                    '-rotate-45 -translate-y-1' : 'translate-y-[5px]'
-                    }`} >
-    </span>    
 
-  </button>
-  
-)
+    const scrollToSection = (id) => {
+        const element = document.getElementById(id);
+        if (element) {
+            element.scrollIntoView({ behavior: "smooth" });
+        }
+        setIsOpen(!isOpen);
+    };
+
+    
+
+    const handleClick = () => {
+        setIsOpen(!isOpen);
+       
+    };
+
+    return (
+        <div className='relative md:hidden'>
+            <button onClick={handleClick}
+                className="flex flex-col justify-center items-center">
+                <span className={`bg-northBeige block transition-all duration-300 ease-out 
+                    h-1 w-[35px] rounded-sm ${isOpen ?
+                        'rotate-45 translate-y-1' : '-translate-y-[5px]'
+                    }`} >
+                </span>
+                <span className={`bg-northBeige block transition-all duration-300 ease-out 
+                    h-1 w-[35px] rounded-sm my-[ ${isOpen ?
+                        'opacity-0' : 'opacity-100'
+                    }`} >
+                </span>
+                <span className={`bg-northBeige block transition-all duration-300 ease-out 
+                    h-1 w-[35px] rounded-sm ${isOpen ?
+                        '-rotate-45 -translate-y-1' : 'translate-y-[5px]'
+                    }`} >
+                </span>
+
+            </button>
+
+            
+
+                <div className={`side-nav ${isOpen ? 'showNav' : 'hideNav'}`}>
+                        <nav>
+                        <div className="my-6">
+                            <span className='header-links cursor-pointer' onClick={() => scrollToSection("services")}>Services</span>
+                        </div>
+                        <div className="my-6">
+                            <span className=' header-links cursor-pointer' onClick={() => scrollToSection("about")}>About</span>
+                        </div>
+                        <div className="my-6">
+                            <span className=' header-links cursor-pointer' onClick={() => scrollToSection("gallery")}>Gallery</span>
+                        </div>
+                        <div className="my-6">
+                            <span className=' header-links cursor-pointer' onClick={() => scrollToSection("process")}>Process</span>
+                        </div>
+                        <div className="my-6">
+                            <span className=' header-links cursor-pointer' onClick={() => scrollToSection("quiz")}>Guide</span>
+                        </div>
+                        <div className="my-6">
+                            <span className=' header-links cursor-pointer' onClick={() => scrollToSection("contact")}>Contact</span>
+                        </div>
+                    </nav>
+                </div>
+                
+        </div >
+
+    )
 };
